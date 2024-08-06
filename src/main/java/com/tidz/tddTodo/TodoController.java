32b4356a736c1fb7f72d26862913e94a7231a0c2
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-06 16:10:50 -0300

    create addTodo route for TodoController class

--- src/main/java/com/tidz/tddTodo/TodoController.java
+++ src/main/java/com/tidz/tddTodo/TodoController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tidz.tddTodo.entity.Todo;
@@ -29,6 +32,11 @@ public class TodoController {
 	public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
 		Optional<Todo> todo = this.todoService.getTodoById(id);
 		return todo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public Todo addTodo(@RequestBody Todo todo) {
+		return this.todoService.addTodo(todo);
 	}
 
 }
