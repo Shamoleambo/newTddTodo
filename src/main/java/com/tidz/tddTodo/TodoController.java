57708dd58a806fc4348ee836ac0af53ab87662ee
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-06 16:18:49 -0300

    implement delete route to the TodoController class

--- src/main/java/com/tidz/tddTodo/TodoController.java
+++ src/main/java/com/tidz/tddTodo/TodoController.java
@@ -5,13 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tidz.tddTodo.entity.Todo;
@@ -48,6 +48,16 @@ public class TodoController {
 			todo.setTitle(updatedTodo.getTitle());
 			todo.setCompleted(updatedTodo.isCompleted());
 			return ResponseEntity.ok(this.todoService.updateTodo(todo));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping
+	public ResponseEntity<Void> deleteTodoById(@PathVariable Long id) {
+		if (this.todoService.getTodoById(id).isPresent()) {
+			this.todoService.deleteTodoById(id);
+			return ResponseEntity.noContent().build();
 		} else {
 			return ResponseEntity.notFound().build();
 		}
