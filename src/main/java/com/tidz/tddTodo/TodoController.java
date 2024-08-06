e3aa226253c076c9d0eb0c4904e33a539f09211e
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-06 16:09:27 -0300

    create route to get a single Todo

--- src/main/java/com/tidz/tddTodo/TodoController.java
+++ src/main/java/com/tidz/tddTodo/TodoController.java
@@ -1,9 +1,12 @@
 package com.tidz.tddTodo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +24,11 @@ public class TodoController {
 	public List<Todo> getAllTodos() {
 		return this.todoService.getAllTodos();
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+		Optional<Todo> todo = this.todoService.getTodoById(id);
+		return todo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
 }
